791d9e77cab3bd05338d83e64e4357b24fe75405
Getange <emmanuelgetange48@gmail.com>
2023-04-21 16:40:51 +0300

    js.tutorial commit

---
+++ html and css/entrie.js
@@ -0,0 +1,5 @@
+let student=['Emmanuel','Joy','Dbriane','Antoh']
+  let combine=student.entries();//indexing function to form a 2-dimensional array.
+   for(let entry of combine){
+    console.log(entry);
+   }
