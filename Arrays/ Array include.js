8bca78bfc37dd11581dcd91de77cab8f66bd2be7
Getange <emmanuelgetange48@gmail.com>
2023-05-20 12:33:43 +0300

    array include

--- Arrays/ Array include.js
+++ Arrays/ Array include.js
@@ -1,1 +1,16 @@
 //this method checks whether the array contain spacified element.
+let language = ["Javascript","Java","Golang","Python"]
+//lets check whether the element exists in the array.
+let check=language.includes("Golang");
+console.log(check);//return true.
+
+let check2=language.includes("Ruby");
+console.log(check2);//returns false.
+
+//include method for case sensitivite.
+let check3=language.includes("python");
+console.log(check3);// returns false.
+
+//include using 2 parameters.
+let check4=language.includes("Python",3 );
+console.log(check4);
