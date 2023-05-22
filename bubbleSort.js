63ed60df4bc198c1c6f8d0f40abb19a70a86ca42
Getange <emmanuelgetange48@gmail.com>
2023-05-22 12:40:24 +0300

    last commit

--- bubbleSort.js
+++ bubbleSort.js
@@ -21,8 +21,8 @@ console.log("Sorted array:", sortedArr); // [1, 2, 3, 7, 9]
 
 
 function fizzBuzz(){
-    for(let i=1;i<=100; i++){
-        if(i %3 === 0 && i%5===0){
+    for(let i=1; i<=100; i++){
+        if(i%3 === 0 && i%5===0){
             console.log("FizzBuzz")
         }else if(i%3===0){
             console.log("Fizz")
