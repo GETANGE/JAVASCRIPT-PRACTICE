485140a8e05b89daafee4375c7f408940abb6a5d
Getange <emmanuelgetange48@gmail.com>
2023-06-02 22:56:43 +0300

    ADD:array push and isArray

---
+++ Arrays/Array push.js
@@ -0,0 +1,9 @@
+//this method is used to push new elements to the end of the array.
+let cities=['London','New York','Nairobi','Kisumu']
+cities.push('Kisii');
+console.log(cities);
+
+var languages = ["JavaScript", "Python", "Java", "Lua"];
+let count=languages.push('Rust');
+console.log("Added new element in the list: "+" "+languages);
+console.log("Number of elemnts after pushing new element is:"+" "+count);
---
+++ Arrays/isArray.js
@@ -0,0 +1,6 @@
+//this method is used to test whether the value passed is an array.
+let numbers=[1,2,3,4,5]
+console.log(Array.isArray(numbers));//returns true 
+
+let text='Javascript'
+console.log(Array.isArray(text));//returns false.
