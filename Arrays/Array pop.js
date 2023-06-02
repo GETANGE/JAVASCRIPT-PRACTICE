83ab475a1d574a543f7addb21179bbfb42adbbd5
Getange <emmanuelgetange48@gmail.com>
2023-06-02 23:36:17 +0300

    ADD:array pop

---
+++ Arrays/Array pop.js
@@ -0,0 +1,6 @@
+//this array removes the last element from the array
+let city=['New York', 'Chicago','San Francisco','Nairobi','Nyeri']
+
+//remove the last element from the array.
+var removeCity=city.pop()
+console.log("The city to be dispaced from the array is :"+" "+removeCity)
