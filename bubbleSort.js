0945dd3e16a0a0d54caf2d78d14d36525d0a1f15
Getange <emmanuelgetange48@gmail.com>
2023-06-04 10:59:37 +0300

    ADD:code refuctering

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,9 +3,9 @@ function bubbleSort(arr) {
   for (let i = 0; i < n; i++) {
     for (let k = 0; k < n - i - 1; k++) {
       if (arr[k] > arr[k + 1]) {
-        const temp = arr[k];
+        const temp1 = arr[k];
         arr[k] = arr[k + 1];
-        arr[k + 1] = temp;
+        arr[k + 1] = temp1;
       }
     }
   }
@@ -19,18 +19,3 @@ console.log("Unsorted array:", unsortedArr);
 const sortedArr = bubbleSort(unsortedArr);
 console.log("Sorted array:", sortedArr); // [1, 2, 3, 7, 9]
 
-
-function fizzBuzz(){
-    for(let i=1; i<=100; i++){
-        if(i%3 === 0 && i%5===0){
-            console.log("FizzBuzz")
-        }else if(i%3===0){
-            console.log("Fizz")
-        }else if(i%5===0){
-            console.log("Buzz")
-        }else{
-            console.log(i)
-        }
-      }
- }
- fizzBuzz();
