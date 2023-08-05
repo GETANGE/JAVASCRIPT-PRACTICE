1e29a9385afb29b6ac4011082f1793eb84592683
Getange <emmanuelgetange48@gmail.com>
2023-08-05 15:32:16 +0300

    ADD: OOP Udemy

--- OOP Javascript/Mosh/index.js
+++ OOP Javascript/Mosh/index.js
@@ -15,20 +15,33 @@ circle.draw();
 console.log(circle);
 
 //using factory reconstructor functions.
-function createPerson(firstName, lastName){
+function createPerson(firstName, lastName,middleName){
     let person={
         firstName,
+        middleName,
         lastName,
         getFullName: function(){
-            return `${firstName} ${lastName}`;
+            return `${firstName} ${lastName} ${middleName}`;
         }
     }
     return person;
 }
 //create a new person.
-const person1 = createPerson("Emmanuel","Getange");
-const person2 = createPerson("JoyElizabeth","Muthoni");
+const person1 = createPerson("Emmanuel","Mokomba","Getange");
+const person2 = createPerson("JoyElizabeth","Kiboi","Muthoni");
 
 //display the person created.
 console.log(person1.getFullName());
 console.log(person2.getFullName());
+
+//using the constructor functions.
+function Car(owner, name, regNo){
+    this.owner = owner;
+    this.name = name;
+    this.regNo = regNo;
+}
+//create a new object .
+const Car1 = new Car("Emmanuel", "Mitsubishi","KDK 123B");
+console.log(Car1.owner);
+console.log(Car1.name);
+console.log(Car1.regNo);
