1981305e26ce479127b18d1b7abdca00c06455dc
Getange <emmanuelgetange48@gmail.com>
2023-09-13 11:30:17 +0300

    ADD: Escape functionality

--- Jonas.io/doms2.js
+++ Jonas.io/doms2.js
@@ -27,3 +27,14 @@ const closeModal = function () {
  btnCloseModal.addEventListener('click', closeModal);
 
  overlay.addEventListener('click', closeModal);
+
+ // adding the 'Esc' functionality .
+ document.addEventListener('keydown',function(e){
+   console.log(e.key);
+
+    if(e.key === 'Escape'){
+      if(!modal.classList.contains('hidden')){
+         closeModal();
+      }
+    }
+ })
